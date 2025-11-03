0f85dfc5c41e5a4103374f99497f31a40449aedf
Pavel Ryabykh <pryabykh@gmail.com>
2025-11-03 21:19:04 +0300

    init commit

---
+++ src/main/java/com/pryabykh/yandex_praktikum_kafka_1/YandexPraktikumKafka1Application.java
@@ -0,0 +1,13 @@
+package com.pryabykh.yandex_praktikum_kafka_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexPraktikumKafka1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YandexPraktikumKafka1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pryabykh/yandex_praktikum_kafka_1/YandexPraktikumKafka1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pryabykh.yandex_praktikum_kafka_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YandexPraktikumKafka1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
