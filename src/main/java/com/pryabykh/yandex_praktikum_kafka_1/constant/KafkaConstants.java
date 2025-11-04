576b1051a19102f1ecc34ec0c38126542ebdd20e
Pavel Ryabykh <pryabykh@gmail.com>
2025-11-04 15:49:23 +0300

    Добавить третьего брокера Kafka

--- src/main/java/com/pryabykh/yandex_praktikum_kafka_1/constant/KafkaConstants.java
+++ src/main/java/com/pryabykh/yandex_praktikum_kafka_1/constant/KafkaConstants.java
@@ -3,5 +3,5 @@ package com.pryabykh.yandex_praktikum_kafka_1.constant;
 public class KafkaConstants {
 
     public static final String TOPIC_NAME = "current_weather";
-    public static final String BOOTSTRAP_SERVERS = "localhost:29092,localhost:29093";
+    public static final String BOOTSTRAP_SERVERS = "localhost:29092,localhost:29093,localhost:29094";
 }
